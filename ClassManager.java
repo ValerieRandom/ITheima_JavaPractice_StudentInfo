f3c7ca2b255870039239634226af6b98985a98ff
valerie.lai <valerie.lai@yesee.com.tw>
2024-10-04 12:32:07 +0800

    test

--- ClassManager.java
+++ ClassManager.java
@@ -7,7 +7,5 @@ public class ClassManager {
     //創建出由學生組成的集合 就像班級一樣
     private ArrayList<Student> StudentArrayLists = new ArrayList<Student>();
 
-    StudentArrayLists.add(s1);
+
         }
-    }
-}
--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@
 public class Test {
     public static void main(String[] args) {
         // 使用 ClassManager 類的 s1 變數
-        System.out.println(ClassManager.s1);
+        //System.out.println(ClassManager.s1);
     }
 }
